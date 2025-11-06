312345a2d38d22fbb26a1f20b8f019d714c5b734
diego-dcr <diego.rivera@siscoplagas.com.mx>
2025-11-06 10:47:35 -0600

    Nav permissions helpers3

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -21,14 +21,14 @@
     }
 </style>
 
-    <ul class="nav flex-column">
-        @isset($navigation)
-            @foreach ($navigation as $key => $nav)
-                @if(tenant_can($nav['permission']))
-                    <li class="nav-item">
-                        <a class="nav-link navbar-item" href="{{ $nav['route'] }}">{{ $key }}</a>
-                    </li>
-                @endif
-            @endforeach
-        @endisset
-    </ul>
+<ul class="nav flex-column">
+    @isset($navigation)
+        @foreach ($navigation as $key => $nav)
+            @if ($nav['permission'] === null || tenant_can($nav['permission']))
+                <li class="nav-item">
+                    <a class="nav-link navbar-item" href="{{ $nav['route'] }}">{{ $key }}</a>
+                </li>
+            @endif
+        @endforeach
+    @endisset
+</ul>
