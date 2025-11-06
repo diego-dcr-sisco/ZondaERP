c421fca82832323b8beb6f8f520ca0ce28d4a6ca
diego-dcr <diego.rivera@siscoplagas.com.mx>
2025-11-06 12:16:03 -0600

    Nav permissions helpers3.55

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -85,7 +85,7 @@ class OrderController extends Controller
 		$customer_ranges = Customer::where('general_sedes', '!=', 0)->orWhere('service_type_id', 1)->orderBy('name', 'asc')->get();
 		$navigation = $this->navigation;
 
-		dd(auth()->user());
+		dd(auth()->user()->tenant());
 
 		return view(
 			'order.index',
