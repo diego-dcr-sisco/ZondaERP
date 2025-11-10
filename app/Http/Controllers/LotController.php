56badcbb5354545e6fb5dd03329cb79ae455cac9
GriseldaMartinezSisco <griselda.martinez@siscoplagas.mx>
2025-11-10 10:54:49 -0600

    navigation LotController

--- app/Http/Controllers/LotController.php
+++ app/Http/Controllers/LotController.php
@@ -20,17 +20,50 @@ use App\Models\MovementType;
 class LotController extends Controller
 {
     public $navigation = [
-        'Almacenes' => '/stock',
-        'Lotes' => '/lot/index',
-        'Productos' => '/products',
-        'Movimientos' => '/stock/movements',
-        'Consumos en ordenes' => '/stock/movements/orders',
-        'Consumos' => '/consumptions/',
-        // 'Zonas' => '/customer-zones',
-        // 'Pedidos' => '/consumptions',
-        // 'Productos en ordenes' => '/stock/orders-products',
-        //'Estadisticas' => '/stock/analytics',
-        // 'Compras' => '/purchase-requisition/purchases',
+        'Almacenes' => [
+            'route' => '/stock',
+            'permission' => null
+        ],
+        'Lotes' => [
+            'route' => '/lot/index',
+            'permission' => null
+        ],
+        'Productos' => [
+            'route' => '/products',
+            'permission' => null
+        ],
+        'Movimientos' => [
+            'route' => '/stock/movements',
+            'permission' => null
+        ],
+        'Consumos en ordenes' => [
+            'route' => '/stock/movements/orders',
+            'permission' => null
+        ],
+        'Consumos' => [
+            'route' => '/consumptions/',
+            'permission' => null
+        ],
+        /*'Zonas' => [
+            'route' => '/customer-zones',
+            'permission' => null
+        ],
+        'Pedidos' => [
+            'route' => '/consumptions',
+            'permission' => null
+        ],
+        'Productos en ordenes' => [
+            'route' => '/stock/orders-products',
+            'permission' => null
+        ],
+        'Estadisticas' => [
+            'route' => '/stock/analytics',
+            'permission' => null
+        ],
+        'Compras' => [
+            'route' => '/purchase-requisition/purchases',
+            'permission' => null
+        ]*/
     ];
 
     public function index(Request $request)
