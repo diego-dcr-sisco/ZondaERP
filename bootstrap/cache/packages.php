55db0dac3af0d1da411d2a36620907e9172a623a
GriseldaMartinezSisco <griselda.martinez@siscoplagas.mx>
2025-11-11 11:44:48 -0600

    Composer update dev3

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -105,13 +105,13 @@
   ),
   'simplesoftwareio/simple-qrcode' => 
   array (
-    'providers' => 
-    array (
-      0 => 'SimpleSoftwareIO\\QrCode\\QrCodeServiceProvider',
-    ),
     'aliases' => 
     array (
       'QrCode' => 'SimpleSoftwareIO\\QrCode\\Facades\\QrCode',
+    ),
+    'providers' => 
+    array (
+      0 => 'SimpleSoftwareIO\\QrCode\\QrCodeServiceProvider',
     ),
   ),
   'spatie/laravel-backup' => 
